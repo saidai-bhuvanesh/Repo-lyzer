ac88714e03cbfbc5ca51de5cc7805ae2f09051bc
ns_110211 <133208903+navya-1907@users.noreply.github.com>
2026-05-22 11:02:40 +0530

    Add JSON export file writer and intergrate analysis
    Added AnalysisOutput struct and SaveAnalysisJSON function to handle JSON output for analysis results.

--- internal/output/json.go
+++ internal/output/json.go
@@ -30,3 +30,56 @@ func PrintLanguages(langs map[string]int) {
 		fmt.Printf("%-10s %s %.1f%%\n", lang, bar, percent)
 	}
 }
+
+type AnalysisOutput struct {
+	Repo           interface{}    `json:"Repo"`
+	Commits        interface{}    `json:"Commits"`
+	Contributors   interface{}    `json:"Contributors"`
+	FileTree       interface{}    `json:"FileTree"`
+	Languages      map[string]int `json:"Languages"`
+	HealthScore    int            `json:"HealthScore"`
+	BusFactor      int            `json:"BusFactor"`
+	BusRisk        string         `json:"BusRisk"`
+	MaturityScore  int            `json:"MaturityScore"`
+	MaturityLevel  string         `json:"MaturityLevel"`
+}
+
+func SaveAnalysisJSON(
+	path string,
+	repo interface{},
+	commits interface{},
+	contributors interface{},
+	fileTree interface{},
+	langs map[string]int,
+	healthScore int,
+	busFactor int,
+	busRisk string,
+	maturityScore int,
+	maturityLevel string,
+) error {
+
+	data := AnalysisOutput{
+		Repo:          repo,
+		Commits:       commits,
+		Contributors:  contributors,
+		FileTree:      fileTree,
+		Languages:     langs,
+		HealthScore:   healthScore,
+		BusFactor:     busFactor,
+		BusRisk:       busRisk,
+		MaturityScore: maturityScore,
+		MaturityLevel: maturityLevel,
+	}
+
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(path, jsonData, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
